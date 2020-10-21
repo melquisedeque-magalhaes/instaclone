9ed6fab5f5a8d75eac26737cf60c13df2d9f434d
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2020-10-21 17:51:22 -0300

    add editor conf and preddier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-
+// import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  View,
-} from 'react-native';
+import { View, StatusBar } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
 
-const App = () => {
-  return (
-    <>
-      <View />
-    </>
-  );
-};
-
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#111B1A" />
+    <View style={{ flex: 1, backgroundColor: '#111B1A' }} />
+  </>
+);
 
 export default App;
